4218b2d1e2dac928771331d97d03a0e13da414c8
sayamablack <sayamablack@gmail.com>
2013-01-19 13:21:50 +0900

    add enabled button
    無効にしてあるボタンでイベントが発生しないことを確認。

--- Resources/ui/common/FirstView.js
+++ Resources/ui/common/FirstView.js
@@ -1,7 +1,7 @@
 //FirstView Component Constructor
 function FirstView() {
 	//create object instance, a parasitic subclass of Observable
-	var self = Ti.UI.createView();
+	var self = Ti.UI.createView({layout:"vertical"});
 	
 	//label using localization-ready strings from <app dir>/i18n/en/strings.xml
 	var label = Ti.UI.createLabel({
@@ -15,6 +15,24 @@ function FirstView() {
 	//Add behavior for UI
 	label.addEventListener('click', function(e) {
 		alert(e.source.text);
+	});
+	
+	var button = Ti.UI.createButton({
+		title:"button",
+		enabled:false,
+	})
+	self.add(button);
+	button.addEventListener('click', function(e) {
+		alert(e.source.title);
+	});
+	
+	
+	var button2 = Ti.UI.createButton({
+		title:"button2"
+	})
+	self.add(button2);
+	button2.addEventListener('click', function(e) {
+		alert(e.source.title);
 	});
 	
 	return self;
